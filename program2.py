4ccf7b2ae6f0ad9d287412a5f374d18ea5d4635d
Sakshi Pawar <152795092+Pawar-Sakshi-Jagannath@users.noreply.github.com>
2023-12-10 12:12:15 +0530

    Python programs

---
+++ program2.py
@@ -0,0 +1,5 @@
+total=1
+list1=[1,2,3,4,5]
+for ele in range(0,len(list1)):
+    total=total*list1[ele]
+print("Sum of all elements :",total)
---
+++ program3.py
@@ -0,0 +1,6 @@
+list1=[80,27,84,91]
+print("List elements are :")
+for i in range(0,len(list1)):
+    print(list1[i],sep=" ",end=" ")
+print()
+print("Maximum number:",max(list1))
